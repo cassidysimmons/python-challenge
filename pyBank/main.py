225c1fcf19a4257e3aa5b391f950f06762e2ebc7
Cassidy Simmons <cassidy1231@yahoo.com>
2023-11-15 14:34:55 -0600

    finally figured out average change*

--- pyBank/main.py
+++ pyBank/main.py
@@ -4,37 +4,69 @@
 # 4. greatest increase + date
 # 5. greatest decrease + date
 
-# modules
-import os
 import csv
+import os
 
-csvpath = os.path.join("..","pyBank","resources","budget_data.csv")
+csvpath = os.path.join("..", "pyBank", "resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
-    csvheader = next(csvreader)
+    header = next(csvreader)
 
-# header*
     print("financial analysis")
-    print("--------------------------------------------------")
+    print("----------------------------------------")
 
-# set beginning value to zero
+# print the number of months (rows) in the file
+    months = (len(list(csvreader)))
+    print("total months:" , months)
+
+# start back at row one...
+    csvfile.seek(0)
+    next(csvreader)
+
+    # sets the beginning total value to zero
     total = 0
 
     for col in csvreader:
-# -- only prints row one ??? --
-        print(col)
 
-# print the number of months (rows) in the file
-        months = (len(list(csvreader)))
-        print("total months:" , months + int(1))
-
-
-# -- not working, only printing row one value ?? --
         total += int(col[1])
-        print("total: $" , total)
+    print("total: $" , total)
+# ============================================================
 
-# -- how do i pull a specific 'cell' value, need last value of col 1 ?? --
-        change = int(col[1]) - int(col[1])
-        print("average change: $" , change)
+# start back to row 1...
+    csvfile.seek(0)
+
+# !! creates a list of the rows in the file !!
+    listofrows = list(csvreader)
+
+# pulls the 2* row from the list we created, with an index of 1 (2nd value*)
+    firstrow = listofrows[1][1]
+    lastrow = listofrows[86][1]
+
+    change = int(lastrow) - int(firstrow)
+    print("average change: $" , round(change / 85, 2))
+# ==============================================================
+
+    csvfile.seek(0)
+    next(csvreader)
+
+    netlist = []
+    firstrow = next(csvreader)
+    prevnet = int(firstrow[1])
+
+    for col in csvreader:
+        change = int(col[1]) - prevnet
+        netlist += [change]
+        increase = max(netlist)
+        decrease = min(netlist)
+
+    averagechange = sum(netlist) / len(netlist)
+    print(averagechange)
+
+
+# =============================================================
+
+    print("greatest increase in profits:", increase)
+    print("greatest decrease in profits:", decrease)
+
+
