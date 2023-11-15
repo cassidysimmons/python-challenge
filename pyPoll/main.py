63c08ea7771901c0d9f722b5c3965dfa345c3571
Cassidy Simmons <cassidy1231@yahoo.com>
2023-11-15 16:22:47 -0600

    final commit (completed assignment)

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -17,12 +17,59 @@ with open(csvpath) as csvfile:
     print("total votes:", votes)
     print("---------------------------------")
 
+#=================================================================
 # return to the first row...
     csvfile.seek(0)
     next(csvreader)
 
+    casperlist = []
+    row = next(csvreader)
+
     for col in csvreader:
-        for col[2] == "Charles Casper Stockham":
-            casper = len(col[2])
-        
-    print(casper)
+        if col[2] == "Charles Casper Stockham":
+            
+            casperlist += [row]
+
+    print("Charles Casper Stockham:", 
+           round(((len(casperlist)/ votes) *100), 3) , "%" ,
+    "(" , len(casperlist) , ")" )
+# ===============================================================
+# return to the first row...
+    csvfile.seek(0)
+    next(csvreader)
+
+    dianalist = []
+    row = next(csvreader)
+
+    for col in csvreader:
+        if col[2] == "Diana DeGette":
+            
+            dianalist += [row]
+
+    print("Diana DeGette:", 
+           round(((len(dianalist)/ votes) *100), 3) , "%" ,
+    "(" , len(dianalist) , ")" )
+# ===============================================================
+# return to the first row...
+    csvfile.seek(0)
+    next(csvreader)
+
+    raylist = []
+    row = next(csvreader)
+
+    for col in csvreader:
+        if col[2] == "Raymon Anthony Doane":
+            
+            raylist += [row]
+
+    print("Raymon Anthony Doane:", 
+           round(((len(raylist)/ votes) *100), 3) , "%" ,
+    "(" , len(raylist) , ")" )
+# ===============================================================
+
+    print("---------------------------------")
+
+# since the percentages are already calculated, we can just look and manually print the winner
+    print("winner: Diana Degette")
+    
+    print("---------------------------------")
