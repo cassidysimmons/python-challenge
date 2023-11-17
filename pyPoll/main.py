bb7df7a4487a6aa5c67f04df99c79ce4099b6d81
Cassidy Simmons <cassidy1231@yahoo.com>
2023-11-16 22:14:40 -0600

    added txt export (complete)

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -8,13 +8,20 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
+    output = []
+
+    output.append("election results")
     print("election results")
+    output.append("---------------------------------")
     print("---------------------------------")
 
 # print number of votes (rows) in the file
     rows =list(csvreader)
     votes = len(rows)
+
+    output.append(("total votes:", votes))
     print("total votes:", votes)
+    output.append("---------------------------------")
     print("---------------------------------")
 
 #=================================================================
@@ -29,6 +36,10 @@ with open(csvpath) as csvfile:
         if col[2] == "Charles Casper Stockham":
             
             casperlist += [row]
+
+    output.append(((((("Charles Casper Stockham:", 
+           round(((len(casperlist)/ votes) *100), 3) , "%" ,
+    "(" , len(casperlist) , ")" ))))))
 
     print("Charles Casper Stockham:", 
            round(((len(casperlist)/ votes) *100), 3) , "%" ,
@@ -46,6 +57,10 @@ with open(csvpath) as csvfile:
             
             dianalist += [row]
 
+    output.append(((((("Diana DeGette:", 
+           round(((len(dianalist)/ votes) *100), 3) , "%" ,
+    "(" , len(dianalist) , ")" ))))))
+
     print("Diana DeGette:", 
            round(((len(dianalist)/ votes) *100), 3) , "%" ,
     "(" , len(dianalist) , ")" )
@@ -62,14 +77,26 @@ with open(csvpath) as csvfile:
             
             raylist += [row]
 
+    output.append(((((("Raymon Anthony Doane:", 
+           round(((len(raylist)/ votes) *100), 3) , "%" ,
+    "(" , len(raylist) , ")" ))))))
+
     print("Raymon Anthony Doane:", 
            round(((len(raylist)/ votes) *100), 3) , "%" ,
     "(" , len(raylist) , ")" )
 # ===============================================================
 
+    output.append("---------------------------------")
     print("---------------------------------")
 
 # since the percentages are already calculated, we can just look and manually print the winner
+    output.append("winner: Diana Degette")
     print("winner: Diana Degette")
     
+    output.append('---------------------------------')
     print("---------------------------------")
+
+file = open(os.path.join("analysis", "pyPoll analysis.txt"), "w")
+
+for o in output:
+    file.write (str(o) + "\n")
